0f9f4db0d9155a98fcdfc644ef1b05873d3d98f8
goyumsamuditha <goyumsamuditha@gmail.com>
2025-09-16 08:22:34 +0530

    Implement main.py with executable details

---
+++ question1_university_system/main.py
@@ -0,0 +1,50 @@
+# main.py
+from person import Staff
+from student import UndergraduateStudent, GraduateStudent
+from faculty import Professor, Lecturer, TA
+from department import Department, Course
+
+def main():
+    # Create a department
+    department_d1 = Department("Computer Science") # Department for CS courses
+
+    # Create courses
+    course_c01 = Course("PR100", "Introduction to Programming", 2) # Programming course
+    course_c02 = Course("ML200", "Machine Learning", 2, prerequisites=["PR100"]) # ML course with PR100 as prerequisite
+
+    # Assign courses to department
+    department_d1.add_course(course_c01) # Add PR100 course to CS department
+    department_d1.add_course(course_c02) # Add ML200 course to CS department
+
+    # Create faculty
+    lecturer_l001 = Lecturer("Nimal Perera", "MSc in Programming", 3) # Lecturer for PR100 course
+    lecturer_l002 = Professor("Lihini Fernando", "PhD in Data Science", 2) # Professor for ML200 course
+    teaching_assistant_ta001 = TA("Thilan Jayasinghe", "BSc in Data Science", 1) # TA for PR100 course
+    staff_st001 = Staff("Kaushal Mendis") # Staff member
+
+    # Assign faculty to department
+    department_d1.add_faculty(lecturer_l001) # Add Lecturer to CS department
+    department_d1.add_faculty(lecturer_l002) # Add Professor to CS department
+
+
+    # Create students
+    student_s001 = UndergraduateStudent("Mike Ross", "CS001", "CS") # Undergraduate student in CS
+    student_s002 = GraduateStudent("Rachel Zane", "CS002", "CS") # Graduate student in CS
+
+    # Enroll students in courses
+    student_s001.enroll_course(course_c01) # should succeed
+    student_s001.enroll_course(course_c02)  # prerequisite check
+
+    student_s002.enroll_course(course_c02)  # should fail due to missing prerequisite
+
+    # Set GPA and show academic status
+    student_s001.set_gpa(3.8) # Set GPA for undergraduate student
+    print(f"{student_s001.name}: GPA: {student_s001.get_gpa()}, Status: {student_s001.get_academic_status()}") # Show academic status
+
+    # Show responsibilities
+    people = [lecturer_l001,lecturer_l002, student_s001, student_s002, teaching_assistant_ta001, staff_st001] # List of all people
+    for person in people:
+        print(f"{person.name}: {person.get_responsibilities()}") # Print responsibilities of each person
+
+if __name__ == "__main__":
+    main()
